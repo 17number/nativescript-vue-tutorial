b8e4e20a7121c51d19c77ef70768d6bb2b31ac09
r17n <4716989+17number@users.noreply.github.com>
2020-02-17 12:37:20 +0900

    feat: push notification example: ios with firebase cloud messaging

--- app/main.js
+++ app/main.js
@@ -7,6 +7,29 @@ if(TNS_ENV !== 'production') {
   Vue.use(VueDevtools)
 }
 
+const appSettings = require('tns-core-modules/application-settings');
+import firebase from 'nativescript-plugin-firebase';
+
+// プッシュ通知用トークンの更新
+const onPushTokenReceived = token => {
+  if (!token) {
+    console.error(`token is undefined or null: ${token}`);
+    return;
+  }
+  console.log({token});
+  appSettings.setString("fcm_token", token);
+};
+
+// Firebase プラグイン 初期化
+firebase.init({
+  // プッシュ通知用トークンの更新
+  onPushTokenReceivedCallback: onPushTokenReceived,
+})
+  .then(
+    () => console.log("firebase.init success"),
+    (error) => console.error(`firebase.init error: ${error}`)
+  );
+
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
